0322988b4135c01c3283485b7c407575db6c4435
Nur <furiousnur@gmail.com>
2023-07-04 00:33:35 +0600

    sub total added

--- src/products.js
+++ src/products.js
@@ -1,11 +1,11 @@
 export const products = [
-    { id: 1, name: 'Product 1', price: 10.99 },
-    { id: 2, name: 'Product 2', price: 19.99 },
+    { id: 1, name: 'Product 1', price: 50.99 },
+    { id: 2, name: 'Product 2', price: 49.99 },
     { id: 3, name: 'Product 3', price: 8.99 },
-    { id: 4, name: 'Product 4', price: 14.99 },
-    { id: 5, name: 'Product 5', price: 12.99 },
-    { id: 6, name: 'Product 6', price: 8.99 },
+    { id: 4, name: 'Product 4', price: 94.99 },
+    { id: 5, name: 'Product 5', price: 32.99 },
+    { id: 6, name: 'Product 6', price: 60.99 },
     { id: 7, name: 'Product 7', price: 14.99 },
-    { id: 8, name: 'Product 8', price: 12.99 },
-    { id: 9, name: 'Product 9', price: 12.99 },
+    { id: 8, name: 'Product 8', price: 992.99 },
+    { id: 9, name: 'Product 9', price: 72.99 },
 ];
